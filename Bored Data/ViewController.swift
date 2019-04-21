bcf30f4feec34a17f49392664d1ce9809b34fa59
Sighmox <Sighmox@github.com>
2019-04-21 16:17:01 -0500

    Initial Commit

---
+++ Bored Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bored Data
+//
+//  Created by Paul Baker on 4/21/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
